3bbef495504ac27a8233f5ec0c23a72fa267e8c5
Dev GC Connector <dev@gc-connector.local>
2026-06-04 22:31:04 +0000

    Add root layout for admin page

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'GC Connector',
+  description: 'GlobalControl Modal API and Admin'
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
